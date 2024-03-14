68d935b32a0f8a32c6772a416c388ecbfc2d15dc
dryelm <rinat.gilemshinn@gmail.com>
2024-03-14 17:22:31 +0500

    done 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,95 +1,84 @@
 package com.example.myapplication
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.versionedparcelable.ParcelField
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
+data class Habit(val name: String, val description: String, val priority: String, val type: String, val days: Int, val times: Int) : Parcelable
+
+enum class HabitPriority(val priority: String){
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High"),
+}
 class MainActivity : AppCompatActivity() {
-    var number = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        val habits = intent.getParcelableArrayListExtra("habits", Habit::class.java)
+            ?: ArrayList<Habit>()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState != null) {
-            number = savedInstanceState.getInt("number")
-        }
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = HabitAdapter(habits)
+        Log.d("MainActivity", habits.joinToString())
 
-        else if (intent.hasExtra("number")){
-            number = intent.getIntExtra("number", 0)
-        }
-
-        val mes = "Number: $number"
-        Log.d("MainActivity", "Number: $number")
-
-        val textView = findViewById<TextView>(R.id.number_text)
-        textView.text = mes
-
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("number", number)
+        val addButton = findViewById<FloatingActionButton>(R.id.addButton)
+        addButton.setOnClickListener {
+            val intent = Intent(this, EditHabitActivity::class.java)
+            intent.putParcelableArrayListExtra("habits", habits)
             startActivity(intent)
         }
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("number", number + 1)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        number = savedInstanceState.getInt("number")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("MainActivity", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("MainActivity", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("MainActivity", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("MainActivity", "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("MainActivity", "onDestroy")
-    }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class HabitAdapter(private val habits: ArrayList<Habit>) : RecyclerView.Adapter<HabitAdapter.HabitViewHolder>() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+    class HabitViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.habit_item, parent, false)
+        return HabitViewHolder(view)
     }
+
+    override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
+        val habit = habits[position]
+
+        val nameTextView = holder.view.findViewById<TextView>(R.id.nameTextView)
+        val descriptionTextView = holder.view.findViewById<TextView>(R.id.descriptionTextView)
+        val priorityTextView = holder.view.findViewById<TextView>(R.id.priorityTextView)
+        val typeTextView = holder.view.findViewById<TextView>(R.id.typeTextView)
+        val frequencyTextView = holder.view.findViewById<TextView>(R.id.frequencyTextView)
+
+        holder.view.setOnClickListener {
+            val context = holder.view.context
+            val intent = Intent(context, EditHabitActivity::class.java)
+            intent.putParcelableArrayListExtra("habits", habits)
+            intent.putExtra("index", position)
+            context.startActivity(intent)
+        }
+
+        nameTextView.text = habit.name
+        descriptionTextView.text = habit.description
+        priorityTextView.text = habit.priority
+        typeTextView.text = habit.type
+        frequencyTextView.setText("${habit.times} раз. Через каждые ${habit.days} дней(день)");
+    }
+
+
+    override fun getItemCount() = habits.size
 }
